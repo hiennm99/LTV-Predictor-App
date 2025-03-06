505e88916d1f736e000c2084f81952a0b4f9ba6b
hiennm99 <hiennm11599@gmail.com>
2025-03-06 12:30:29 +0700

    update code

--- frontend/src/secrets/oktaConfigs.tsx
+++ frontend/src/secrets/oktaConfigs.tsx
@@ -4,7 +4,7 @@ import { OktaAuth } from "@okta/okta-auth-js";
 const oktaAuth = new OktaAuth({
   clientId: window._env_?.VITE_OKTA_CLIENT_ID || import.meta.env.VITE_OKTA_CLIENT_ID,
   issuer: window._env_?.VITE_OKTA_ISSUER || import.meta.env.VITE_OKTA_ISSUER,
-  redirectUri: window._env_?.VITE_OKTA_REDIRECT_URI || import.meta.env.VITE_OKTA_REDIRECT_URI,
+  redirectUri: "http://ec2-23-23-79-187.compute-1.amazonaws.com:3003/login/callback",
   scopes: ["openid", "profile", "email"],
   pkce: false,
   tokenManager: {
