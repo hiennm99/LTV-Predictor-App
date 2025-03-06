c54ff33024d941d4e67ae64cefb68c40a344c371
hiennm99 <hiennm11599@gmail.com>
2025-03-06 16:49:02 +0700

    update code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,5 +15,10 @@ export default defineConfig({
     'import.meta.env.VITE_OKTA_CLIENT_ID': JSON.stringify(process.env.VITE_OKTA_CLIENT_ID || ''),
     'import.meta.env.VITE_OKTA_ISSUER': JSON.stringify(process.env.VITE_OKTA_ISSUER || ''),
     'import.meta.env.VITE_OKTA_REDIRECT_URI': JSON.stringify(process.env.VITE_OKTA_REDIRECT_URI || ''),
-  }
+  },
+  allowedHosts: [
+    'ec2-23-23-79-187.compute-1.amazonaws.com',  // Thay bằng hostname EC2 của bạn
+    'ltv.puzzle.sg',
+    'www.ltv.puzzle.sg'
+  ]
 });
