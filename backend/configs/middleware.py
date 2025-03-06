9873e05f0611293a9989111fefa8cdb6a191810d
hiennm99 <hiennm11599@gmail.com>
2025-03-06 15:42:26 +0700

    update code

--- backend/configs/middleware.py
+++ backend/configs/middleware.py
@@ -3,22 +3,16 @@ from fastapi_sqlalchemy import DBSessionMiddleware
 
 def configure_middleware(app):
     origins = [
-        "http://frontend-react",
-        "http://frontend-react-dev", 
         "http://localhost",
-        "http://127.0.0.1",
-        "http://localhost:80",
-        "http://127.0.0.1:80",
-        "http://localhost:3003",  
-        "http://127.0.0.1:3003",
-        "https://4d0a-115-78-237-180.ngrok-free.app",
-        "http://localhost:3001", 
+        "http://localhost:3001",
+        "http://localhost:3003",
+        "https://ltv.puzzle.sg",
     ]
     app.add_middleware(
         CORSMiddleware,
         allow_origins=origins,
         allow_credentials=True,
-        allow_methods=["*"],
+        allow_methods=["POST"],
         allow_headers=["*"],
     )
     app.add_middleware(
