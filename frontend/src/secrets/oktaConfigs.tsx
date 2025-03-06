7343eb4953d604a0bbb3d1c22a7f6342c06f845a
hiennm99 <hiennm11599@gmail.com>
2025-03-06 12:20:23 +0700

    update code

--- frontend/src/secrets/oktaConfigs.tsx
+++ frontend/src/secrets/oktaConfigs.tsx
@@ -1,15 +1,14 @@
+
 import { OktaAuth } from "@okta/okta-auth-js";
 
-const oktaAuth = new OktaAuth({
-  clientId: "0oapbmvtyyl2qb7Dv6973",
-  issuer: "https://login.puzzle.sg",
-  redirectUri: window.location.origin + "/login/callback",
+export const oktaAuth = new OktaAuth({
+  clientId: window._env_?.VITE_OKTA_CLIENT_ID || import.meta.env.VITE_OKTA_CLIENT_ID,
+  issuer: window._env_?.VITE_OKTA_ISSUER || import.meta.env.VITE_OKTA_ISSUER,
+  redirectUri: window._env_?.VITE_OKTA_REDIRECT_URI || `${window.location.origin}/login/callback`,
   scopes: ["openid", "profile", "email"],
-  pkce: false,
+  pkce: true,
   tokenManager: {
     autoRenew: true,
     expireEarlySeconds: 30,
   },
 });
-
-export default oktaAuth;
